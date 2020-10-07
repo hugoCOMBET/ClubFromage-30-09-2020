677a2c2c311c193fd68ff86939d1565928c19c21
Hugo COMBET <hugo.combet@saintmichelannecy.fr>
2020-10-07 11:01:10 +0200

    version 07/10

---
+++ htdocs/affichage.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'Model/Autoloader.php';
+Autoloader::register();
+
+//require("model/business/Fromage.php");
+//require("model/business/Membre.php");
+//require("model/business/Pays.php");
+//require("model/business/Avis.php");
+
+$t = array('identifiant'=>1,'Nom'=>'Camembert');
+$camembert = new \model\business\Fromage($t);
+
+$t = array('identifiant'=>2,'Nom'=>'roquefort');
+$roquefort = new \model\business\Fromage($t);
+
+echo $camembert -> _Nom;
+echo $camembert -> _identifiant;
+
+echo $roquefort -> _Nom;
+echo $roquefort -> _identifiant;
